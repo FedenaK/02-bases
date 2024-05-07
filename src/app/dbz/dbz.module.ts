30f2dcb3877e7f09cad02b945c5891ccfc4f7ba1
FedenaK <federiconakazato@gmail.com>
2024-05-07 17:11:23 -0300

    Integración List a dbz.module

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent,
+    MainPageComponent
+  ],
+  exports: [
+    MainPageComponent
+  ],
   imports: [
     CommonModule
   ]
